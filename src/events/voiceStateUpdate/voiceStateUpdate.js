fca193d7de48a358511c74d3e96ce2d853aecfcd
Atineon <41876775+Atineon@users.noreply.github.com>
2025-08-14 15:15:53 +0000

    fix(mega-linter): apply linters fixes

--- src/events/voiceStateUpdate/voiceStateUpdate.js
+++ src/events/voiceStateUpdate/voiceStateUpdate.js
@@ -1,45 +1,43 @@
-const { ChannelType, Events } = require("discord.js");
-const { Category, Channel } = require("../../../config.json");
+const { ChannelType, Events } = require('discord.js')
+const { Category, Channel } = require('../../../config.json')
 
 module.exports = {
-    name: Events.VoiceStateUpdate,
-    async execute(oldState, newState) {
-        const newStateChannelName = newState.channel && newState.channel.name;
-        const oldStateChannelName = oldState.channel && oldState.channel.name;
-        const joiningUser = newState.member;
-        const leavingUser = oldState.member;
+  name: Events.VoiceStateUpdate,
+  async execute (oldState, newState) {
+    const newStateChannelName = newState.channel && newState.channel.name
+    const oldStateChannelName = oldState.channel && oldState.channel.name
+    const joiningUser = newState.member
+    const leavingUser = oldState.member
 
-        if (newStateChannelName === Channel) {
-            await newState.guild.channels.create({
-                name: joiningUser.user.displayName,
-                type: ChannelType.GuildVoice,
-                parent: newState.guild.channels.cache.find(
-                    (channel) =>
-                        channel.name === Category &&
+    if (newStateChannelName === Channel) {
+      await newState.guild.channels.create({
+        name: joiningUser.user.displayName,
+        type: ChannelType.GuildVoice,
+        parent: newState.guild.channels.cache.find(
+          (channel) =>
+            channel.name === Category &&
                         channel.type === ChannelType.GuildCategory
-                ),
-            });
-            const newCreatedChannel = newState.guild.channels.cache.find(
-                (channel) =>
-                    channel.name === joiningUser.user.displayName &&
+        )
+      })
+      const newCreatedChannel = newState.guild.channels.cache.find(
+        (channel) =>
+          channel.name === joiningUser.user.displayName &&
                     channel.type === ChannelType.GuildVoice
-            );
-            if (newState.channel)
-                joiningUser.voice.setChannel(newCreatedChannel);
-            else newCreatedChannel.delete();
-        }
-        if (oldStateChannelName === leavingUser.user.displayName) {
-            const oldChannelMemberSize = oldState.channel.members.size;
-            const channelFinder = oldState.guild.channels.cache.find(
-                (channel) => channel.name === leavingUser.user.displayName
-            );
-            if (oldChannelMemberSize > 0) {
-                const userFinder =
-                    oldState.channel.members.first().user.displayName;
-                channelFinder.setName(`${userFinder}`);
-            } else {
-                channelFinder.delete();
-            }
-        }
-    },
-};
+      )
+      if (newState.channel) { joiningUser.voice.setChannel(newCreatedChannel) } else { newCreatedChannel.delete() }
+    }
+    if (oldStateChannelName === leavingUser.user.displayName) {
+      const oldChannelMemberSize = oldState.channel.members.size
+      const channelFinder = oldState.guild.channels.cache.find(
+        (channel) => channel.name === leavingUser.user.displayName
+      )
+      if (oldChannelMemberSize > 0) {
+        const userFinder =
+                    oldState.channel.members.first().user.displayName
+        channelFinder.setName(`${userFinder}`)
+      } else {
+        channelFinder.delete()
+      }
+    }
+  }
+}
